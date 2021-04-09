0402904b92288b7cff62a392401745e77981cad6
thiago <thiagoprocaci@gmail.com>
2021-04-08 21:22:27 -0300

    Application - adicionando logica

--- src/main/java/br/edu/infnet/Application.java
+++ src/main/java/br/edu/infnet/Application.java
@@ -4,6 +4,15 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Ola!");
+        if(args == null) {
+            System.out.println("args null");
+        }
     }
+
+    private void test() {
+
+    }
+
+
 
 }
